ccf0d24db1dc626f8aab3fcb73f35a9953c6eb2e
minsau <minsau2@gmail.com>
2016-10-07 10:42:27 -0500

    alskdalsdn

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('localhost/loginFacebook/fb-callback.php', $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
  ?>
